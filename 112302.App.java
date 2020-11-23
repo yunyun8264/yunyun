d499e9364cd4a6cbbfdd11030ac1608a5bcabb4a
yunyun8264 <74659906+yunyun8264@users.noreply.github.com>
2020-11-23 12:09:12 +0800

    Create 112302.App.java

---
+++ 112302.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class App {
+    public static void main(String[] args) {
+        while (true){
+            try{
+                Thread.sleep(1000);
+                int x=(int)(Math.random()*100);
+                int y=(int)(Math.random()*100);
+                System.out.println("随机坐标：("+x+","+y+")");
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
