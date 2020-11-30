ef088571970f9b47ebae07cb994654da8002c340
yunyun8264 <74659906+yunyun8264@users.noreply.github.com>
2020-11-30 11:45:44 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,13 @@
+package pac;
+
+public interface R {
+	static void TuPian(){
+	String boy_filePath="img/boy.gif";
+	String girl_filePath="img/boy.gif";
+	int [][]array2d= {{1,2,3},{4,5,6}};
+	
+	System.out.println(boy_filePath);
+	System.out.println(girl_filePath);
+	System.out.println(array2d);
+	}
+}
